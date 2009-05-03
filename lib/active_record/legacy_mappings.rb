1feeeebdf0932f2ec3d50ee8df1895ded1a46fd5
Reid MacDonald <reid@laruby.com>
2009-05-03 15:41:46 -0700

    initial plugin extraction

---
+++ lib/active_record/legacy_mappings.rb
@@ -0,0 +1,93 @@
+module ActiveRecord
+  module LegacyMappings
+    def self.included(base)
+      base.extend(PluginMethods)
+    end
+
+    module PluginMethods
+      def uses_legacy_mappings mappings = {}
+        self.class_inheritable_hash :legacy_mappings
+        self.legacy_mappings = mappings
+
+        self.extend(ClassMethods)
+        self.send(:include, InstanceMethods)
+        normalize_legacy_field_methods
+      end
+    end
+
+    module ClassMethods
+      def column_methods_hash
+        @dynamic_methods_hash ||= column_names.inject(Hash.new(false)) do |methods, attr|
+          attr_name = attr.to_s
+          updated_attr = self.legacy_mappings[attr.to_sym] || attr_name
+          returning(methods) do |m|
+            m[updated_attr] = attr_name
+            m["#{updated_attr}=".to_sym] = attr_name
+            m["#{updated_attr}?".to_sym] = attr_name
+            m["#{updated_attr}_before_type_cast".to_sym] = attr_name
+          end
+        end
+      end
+
+      def merge_conditions(*conditions)
+        segments = []
+
+        map_legacy_conditions(conditions).each do |condition|
+          unless condition.blank?
+            sql = sanitize_sql(condition)
+            segments << sql unless sql.blank?
+          end
+        end
+
+        "(#{segments.join(') AND (')})" unless segments.empty?
+      end
+
+      protected
+        def normalize_legacy_field_methods
+          column_names.each do |original_attr|
+            next if original_attr == primary_key
+            next unless updated_attr = self.legacy_mappings[original_attr.to_sym]
+
+            define_method(updated_attr) do
+              read_attribute(original_attr)
+            end
+
+            define_method("#{updated_attr}=") do |value|
+              write_attribute(original_attr, value)
+            end
+
+            define_method("#{updated_attr}?") do
+              send("#{original_attr}?")
+            end
+          end
+        end
+
+      private
+        def map_legacy_conditions(conditions)
+          conditions.inject([]) do |mapped_conditions,condition|
+            mapped_conditions << map_legacy_condition(condition) unless condition.blank?
+            mapped_conditions
+          end
+        end
+
+        def map_legacy_condition(condition)
+          return nil if condition.blank?
+          if Hash === condition
+            condition.inject({}) do |h,p|
+              k,v = *p
+              h[self.legacy_mappings.index(k.to_sym) || k] = v
+              h
+            end
+          else 
+            condition
+          end
+        end
+    end
+
+    module InstanceMethods
+      def column_for_attribute(name)
+        self.class.columns_hash[(self.legacy_mappings.index(name.to_sym) || name).to_sym]
+      end
+    end
+  end
+end
---
+++ spec/legacy_mappings_spec.rb
@@ -0,0 +1,159 @@
+require File.dirname(__FILE__) + '/spec_helper'
+
+class MockActiveRecord < ActiveRecord::Base
+  set_table_name 'mocks'
+  def self.column_names
+    [:id, :legacy_a, :legacy_b, :legacy_c]
+  end
+  
+  def self.columns
+    column_names.map do |name|
+      ActiveRecord::ConnectionAdapters::Column.new(name, nil)
+    end
+  end
+end
+
+# test our mock for sanity and requirements for testing
+
+describe MockActiveRecord do
+  it "has original column names and can sanitize_sql" do
+    MockActiveRecord.column_names.should == [:id, :legacy_a, :legacy_b, :legacy_c]
+    MockActiveRecord.columns.map(&:name).should == [:id, :legacy_a, :legacy_b, :legacy_c]
+    MockActiveRecord.send(:sanitize_sql, :foo => 'bar').should == "`mocks`.`foo` = 'bar'"
+  end
+end
+
+describe ActiveRecord::Base do
+  it "includes ActiveRecord::LegacyMethods::PluginMethods" do
+    ActiveRecord::Base.methods.should be_member('uses_legacy_mappings')
+  end
+end
+
+describe :uses_legacy_mappings do
+  it "adds legacy_mappings inheritable hash set to mappings" do
+    MockActiveRecord.uses_legacy_mappings mappings = {:legacy_a => :railsy_named_attribute, :legacy_b => :created_on}
+
+    MockActiveRecord.methods.should be_member('legacy_mappings')
+    MockActiveRecord.legacy_mappings.should == mappings
+  end
+
+  it "extends the class and instance methods" do
+    MockActiveRecord.should_receive(:extend).with(ActiveRecord::LegacyMappings::ClassMethods)
+    MockActiveRecord.should_receive(:include).with(ActiveRecord::LegacyMappings::InstanceMethods)
+    MockActiveRecord.should_receive(:normalize_legacy_field_methods).with(no_args)
+
+    MockActiveRecord.uses_legacy_mappings :legacy_a => :foo
+  end
+end
+
+describe ActiveRecord::LegacyMappings do
+  before do
+    @mappings = {:legacy_a => :railsy_named_attribute, :legacy_b => :created_on}
+    MockActiveRecord.uses_legacy_mappings @mappings
+  end
+
+  describe :column_methods_hash do
+    it "adds mappings to column_methods_hash" do
+      column_methods_for('railsy_named_attribute').each do |method|
+        MockActiveRecord.column_methods_hash[method].should == 'legacy_a'
+      end
+    
+      column_methods_for('created_on').each do |method|
+        MockActiveRecord.column_methods_hash[method].should == 'legacy_b'
+      end
+    end
+
+    it "leaves original mappings alone" do
+      column_methods_for('id') do |method|
+        MockActiveRecord.column_methods_hash[method].should == 'id'
+      end
+
+      column_methods_for('legacy_c') do |method|
+        MockActiveRecord.column_methods_hash[method].should == 'legacy_c'
+      end
+    end
+
+    def column_methods_for(column)
+      ["#{column}=", "#{column}?", "#{column}_before_type_cast"].map(&:to_sym)
+    end
+  end
+
+  describe :merge_conditions do
+    describe :map_legacy_condition do
+      it "remains nil when nil" do
+        MockActiveRecord.send(:map_legacy_condition, nil).should be_nil
+      end
+
+      it "remains the original when not a hash" do
+        MockActiveRecord.send(:map_legacy_condition, 'foo').should == 'foo'
+      end
+    
+      it "maps legacy and mappings correctly" do
+        MockActiveRecord.send(:map_legacy_condition, :id => 1, :railsy_named_attribute => 'foo').should == {:id => 1, :legacy_a => 'foo'}
+      end
+    end
+
+    describe :map_legacy_conditions do
+      it "removes nils" do
+        MockActiveRecord.send(:map_legacy_conditions, ['a',nil,'b']).should == %w[a b]
+      end
+    
+      it "uses map_legacy_condition to create array" do
+        MockActiveRecord.should_receive(:map_legacy_condition).and_return('foo', 'bar')
+        MockActiveRecord.send(:map_legacy_conditions, %w[a b]).should == %w[foo bar]
+      end
+    
+    end
+    
+    it "uses map_legacy_conditions to create conditions" do
+      MockActiveRecord.should_receive(:map_legacy_conditions).and_return([{:foo => 1}, {:bar => 2}])
+      MockActiveRecord.merge_conditions.should == '(`mocks`.`foo` = 1) AND (`mocks`.`bar` = 2)'
+    end
+  end
+
+  describe :normalize_legacy_field_methods do
+    it "doesn't create methods for primary key" do
+      field_methods(:id).each do |arg|
+        MockActiveRecord.should_not_receive(:define_method).with(arg)
+      end
+      MockActiveRecord.send(:normalize_legacy_field_methods)
+    end
+
+    it "doesn't create methods for original columns" do
+      field_methods(:legacy_c).each do |arg|
+        MockActiveRecord.should_not_receive(:define_method).with(arg)
+      end
+      MockActiveRecord.send(:normalize_legacy_field_methods)
+    end
+
+    it "creates methods for legacy mapping columns" do
+      (field_methods(:railsy_named_attribute) + field_methods(:created_on)).each do |arg|
+        MockActiveRecord.should_receive(:define_method).with(arg)
+      end
+      MockActiveRecord.send(:normalize_legacy_field_methods)
+    end
+    
+    def field_methods(column)
+      [column, "#{column}=", "#{column}?"]
+    end
+  end
+
+  describe :column_for_attribute do
+    before do
+      @mock = MockActiveRecord.new
+    end
+    
+    it "can get column (name) for original columns" do
+      MockActiveRecord.column_names.each do |column_name|
+        @mock.column_for_attribute(column_name).name.should == column_name
+      end
+    end
+
+    it "can get column (name) for legacy mappings" do
+      @mappings.each do |column_name, mapping_name|
+        @mock.column_for_attribute(mapping_name).name.should == column_name
+      end
+    end
+  end
+end
+
